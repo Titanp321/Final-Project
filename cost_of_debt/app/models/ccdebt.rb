3613c75cdfc6fddfddce4855a7ce1fb196888134
Titanp321 <rmcgovern2017@kellogg.northwestern.edu>
2016-06-09 14:37:28 -0500

    Added Validations

--- cost_of_debt/app/models/ccdebt.rb
+++ cost_of_debt/app/models/ccdebt.rb
@@ -1,2 +1,10 @@
 class Ccdebt < ActiveRecord::Base
+  validates :accoutname, :presence => true
+
+  validates :user_id, :presence => true
+
+  validates :debt_apr, :presence => true
+
+  validates :debt_balance, :presence => true
+
 end
