ffb463bbd49fa06d245f25869827efaf36361c6d
Titanp321 <rmcgovern2017@kellogg.northwestern.edu>
2016-06-09 16:31:32 -0500

    added some formatting
    Site is functional at this point; no user input

--- cost_of_debt/app/models/ccdebt.rb
+++ cost_of_debt/app/models/ccdebt.rb
@@ -1,10 +1,7 @@
 class Ccdebt < ActiveRecord::Base
-  validates :accoutname, :presence => true
-
-  validates :user_id, :presence => true
-
-  validates :debt_apr, :presence => true
-
-  validates :debt_balance, :presence => true
+  # validates :accoutname, :presence => true
+  # validates :user_id, :presence => true
+  # validates :debt_apr, :presence => true
+  # validates :debt_balance, :presence => true
 
 end
--- cost_of_debt/app/models/ftdebt.rb
+++ cost_of_debt/app/models/ftdebt.rb
@@ -1,6 +1,6 @@
 class Ftdebt < ActiveRecord::Base
-  validates :accoutname, :presence => true
-  validates :user_id, :presence => true
-  validates :debt_apr, :presence => true
-  validates :debt_balance, :presence => true
+  # validates :accoutname, :presence => true
+  # validates :user_id, :presence => true
+  # validates :debt_apr, :presence => true
+  # validates :debt_balance, :presence => true
 end
