f0dd1784c3b0370c53f6025d8aa4ca9dac1bcce8
Titanp321 <rmcgovern2017@kellogg.northwestern.edu>
2016-06-10 18:05:58 -0500

    Started to Build Calculator
    I started to build out the calculator for looking at the interest and it
    is becoming much more challenging that I had first anticipated.
    1) I created an excel sheet model that will help with the logic flow and
    calculate the numbers I'm interested in
    2) Started a ruby file that I've been running in the command line to
    check my coding
    3) So far I have a basic monthly payment calculator working in ruby.
    
    I'm going to clean up the calculator and try to port it into the
    controller now.

--- cost_of_debt/app/controllers/calculator_controller.rb
+++ cost_of_debt/app/controllers/calculator_controller.rb
@@ -1,6 +1,33 @@
 class CalculatorController < ApplicationController
+require 'finance'
 
   def display
+    # Pull in data from users credit cards and fixterm loans
+    @ccdebts = current_user.ccdebts #pull in rows relevant to current current
+    @ftdebts = current_user.ftdebts
+
+
+    #month_interest_rate = @apr/1200
+
+
+
+      @ftdebts.each do |ftdebt|
+        i += 0 
+        apr_rate = ftdebt.debt_apr/100
+        timeframe = ftdebt.debt_term
+        balance = ftdebt.debt_balance
+
+        monthly_payment = balance * apr_rate/(1-(1+apr_rate)**(-1*timeframe))
+        @monthly_payment = monthly_payment +
+
+
+        @totalInterest = 10
+      end
+
+    #rate = Rate.new(0.0425, :apr, :duration => (30 * 12))
+    # amortization = Amortization.new(250000, rate)
+    # @totalInterest = amortization.payment
+
     render 'main/display'
   end
 end
