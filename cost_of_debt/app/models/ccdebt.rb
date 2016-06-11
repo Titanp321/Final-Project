937bceb65c83896c434c8866849e01a8a280a017
Titanp321 <rmcgovern2017@kellogg.northwestern.edu>
2016-06-10 23:20:20 -0500

    removed user ID from show page

--- cost_of_debt/app/models/ccdebt.rb
+++ cost_of_debt/app/models/ccdebt.rb
@@ -1,5 +1,5 @@
 class Ccdebt < ActiveRecord::Base
-  # validates :accoutname, :presence => true
+   validates :accoutname, :presence => true
   # validates :user_id, :presence => true
   # validates :debt_apr, :presence => true
   # validates :debt_balance, :presence => true
